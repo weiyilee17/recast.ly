e0e1a4ff2742e778bd2ab0286bc321999a225525
Hack Reactor Student <student@hackreactor.com>
2017-07-19 17:36:27 -0700

    Pass VideoList Spec

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+
+    //<div><h5><em>{props.vlist.map( (video, index) => <VideoListEntry video={video} key={index} />)}</em> view goes here</h5></div>
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map( (video, index) => <VideoListEntry video={video} key={index} />)}
   </div>
 );
 
@@ -17,3 +15,6 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+//ReactDOM.render(<VideoList />, document.getElementsByClassName("col-md-5")[0])
--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // TODO: Render the `App` component to the DOM
+// var App = () => (
+//   <div>
+//     <VideoList vlist = {exampleVideoData.slice(0,3)}/>
+//   </div>
+// );
+//
+//
+// var VideoList = (props) => (
+//   <div>
+//     {props.vlist.map( (video, index) => <VideoListEntry video={video} key={index} />)}
+//   </div>
+// );
+//
+//
+// var VideoListEntry = (props) => (
+//   <div>
+//     {props.video.snippet.title}
+//   </div>
+// );
+//
 
+ReactDOM.render(<App />, document.getElementById('app'));
